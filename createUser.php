969f880fedd530bc2f0a60dd6f2c7188ab9dd5b4
EduardoDourado <160256067+EduardoDourado@users.noreply.github.com>
2025-01-13 13:08:19 -0300

    alteraçao

---
+++ createUser.php
@@ -0,0 +1,23 @@
+<?php
+
+$password = $_POST["password"];
+$email = $_POST["email"];
+$usuario = $_POST["usuario"];
+
+if (strlen($password) > 8) 
+echo"password must be at lest 8 characters long.";
+
+if (filter_var($email, FILTER_VALIDATE_EMAIL)) {
+    echo"$email is a valid email adress";
+    $_SESSION['feedback'] = "Email invalido.";
+
+}else {
+    echo "$email is not a valid email adress";
+}
+
+
+
+//header("location: index.php");
+
+
+?>
---
+++ user.php
@@ -0,0 +1,39 @@
+<?php
+session_start();
+$feedback = $_SESSION['feedback'] ?? false;
+unset($_SESSION['feedback']);
+?>
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <div>
+        <form action="createUser.php" method="post">
+            <input type="text" id="name" name="name" placeholder="Seu nome">
+            <input type="password" id="senha" name="senha" placeholder="Sua senha">
+            <input type="text" id="email" name="email" placeholder="Seu email">
+            <input type="submit" value="enviar dados">
+
+        </form>
+    </div>
+    <?php
+
+    if ($feedback != false) {
+        echo "<p>$feedback</p>";
+
+    }
+
+    ?>
+    </div>
+</body>
+
+</html>
